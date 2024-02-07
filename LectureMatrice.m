a7a30cfd1bbbd52e1e4604375e3f7696c7b874c6
Thibault Mougin <102171357+thibaultmougin@users.noreply.github.com>
2024-02-07 15:50:10 +0100

    working

--- LectureMatrice.m
+++ LectureMatrice.m
@@ -1,5 +1,5 @@
-clear all 
-close all 
+clear all;
+close all;
 
 f_echantillonnage=50;  % fréquence d'échantillonage des signaux en MHz
 
@@ -44,11 +44,15 @@ K_tmp = fft(K,nbpt,3);
 
 K_freq = K_tmp(:,:,1:floor(nbpt/2+1));
 
+Kload = load("Kfreq.mat");
+
+K_freq = Kload.K_freq;
+f = Kload.f;
+
 % plot(f,reshape(K_freq(28,49,:),[1,floor(nbpt/2+1)]))
-% figure 
 
-[m, I_fmax] = max(abs(reshape(K_freq(28,49,:),[1,floor(nbpt/2+1)])));
-
+% [m, I_fmax] = max(abs(reshape(K_freq(28,49,:),[1,floor(nbpt/2+1)])));
+I_fmax =13;
 L = 128;
 delta_z = 0.3;
 x_l = [zeros(L,2)];
@@ -66,36 +70,67 @@ dx = (xmax-xmin)/Nx;
 dy = (ymax-ymin)/Ny;
 
 img = zeros(Ny,Nx);
+img2 = zeros(Ny,Nx);
+img3 = zeros(Ny,Nx);
+img4 = zeros(Ny,Nx);
+img5 = zeros(Ny,Nx);
+img6 = zeros(Ny,Nx);
 
+% 
 % for f_i=1:length(f)
 %     
-%     [U,S,V]=svd(K(:,:,f_i));
-%     scatter(f(f_i),diag(S),"filled","blue")
-%     grid on 
-%     hold on
+% %     [U,S,V]=svd(K(:,:,f_i));
+%      S(f_i,:) = svd(K_freq(:,:,f_i));
+% 
+% %      scatter(f(f_i),S,"filled","blue")
+% %     hold on 
+% %     grid on
 %     
 % end
+% 
+% for i=1:length(S(1,:))
+%     plot(f,S(:,i))
+%     hold on
+%     grid on
+% end
+% 
+% 
 % xlabel("Fréquence (MHz)")
 % ylabel("Valeurs singulières")
-% figure
 
-[U,S,V]=svd(K(:,:,I_fmax));
+
+[U,S,V]=svd(K_freq(:,:,12));
 
 for i=1:Nx
     for j=1:Ny
 
         z = [xmin+dx*i;ymin+dy*j];
 
-%         n=32;
+        n=64;
 % 
 %         img(j,i) = img(j,i)+I(n,K_freq(n,:,I_fmax),z,f(I_fmax));
+% 
+%         img(j,i) = retro(V(:,1),z,f(12))+retro(V(:,2),z,f(12))+retro(V(:,3),z,f(12))+retro(V(:,4),z,f(12))+retro(V(:,5),z,f(12));
+            img1(j,i)=retro(V(:,1),z,f(12));
+            img2(j,i)=retro(V(:,2),z,f(12));
+            img3(j,i)=retro(V(:,3),z,f(12));
+            img4(j,i)=retro(V(:,4),z,f(12));
+            img5(j,i)=retro(V(:,5),z,f(12));
+            img6(j,i)=retro(V(:,6),z,f(12));
 
-        img(j,i) = retro(V(:,2),z,f(I_fmax));
 
     end 
 end
 
+subplot(1,6,1), image(abs(img1),'CDataMapping','scaled')
+subplot(1,6,2), image(abs(img2),'CDataMapping','scaled')
+subplot(1,6,3), image(abs(img3),'CDataMapping','scaled')
+subplot(1,6,4), image(abs(img4),'CDataMapping','scaled')
+subplot(1,6,5), image(abs(img5),'CDataMapping','scaled')
+subplot(1,6,6), image(abs(img6),'CDataMapping','scaled')
 
+colorbar
+% 
+% image(abs(img),'CDataMapping','scaled')
+% colorbar 
 
-image(abs(img),'CDataMapping','scaled')
-colorbar 
